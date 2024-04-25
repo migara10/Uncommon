cc4851e272fcd3f1fe6d2d96c13df1371f2f5bfc
migara10 <migaraten@gmail.com>
2024-04-26 02:50:46 +0300

    add shadcn ui material

---
+++ client/src/views/NavBar.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./../components/ui/button"
+
+
+const NavBar = () => {
+  return (
+    <div>NavBar
+        <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default NavBar
